b9f79d50268ca9077c28366f44ab5014728fbe7a
Jake Dawkins <dawkinsjh@gmail.com>
2016-11-18 00:21:03 -0500

    updated data file

--- imports/data.js
+++ imports/data.js
@@ -1,16 +1,11 @@
 const data = [
-  {title: "confused", thumbnailURI: "https://media.giphy.com/media/3oEjHFz13et9fQKFb2/giphy.gif"},
-  {title: "confused2", thumbnailURI: "https://media.giphy.com/media/3oEjHFz13et9fQKFb2/giphy.gif"},
-  {title: "confused3", thumbnailURI: "https://media.giphy.com/media/3oEjHFz13et9fQKFb2/giphy.gif"},
-  {title: "confused4", thumbnailURI: "https://media.giphy.com/media/3oEjHFz13et9fQKFb2/giphy.gif"},
-  {title: "confused5", thumbnailURI: "https://media.giphy.com/media/3oEjHFz13et9fQKFb2/giphy.gif"},
-  {title: "confused6", thumbnailURI: "https://media.giphy.com/media/3oEjHFz13et9fQKFb2/giphy.gif"},
-  {title: "confused7", thumbnailURI: "https://media.giphy.com/media/3oEjHFz13et9fQKFb2/giphy.gif"},
-  {title: "confused8", thumbnailURI: "https://media.giphy.com/media/3oEjHFz13et9fQKFb2/giphy.gif"},
-  {title: "confused9", thumbnailURI: "https://media.giphy.com/media/3oEjHFz13et9fQKFb2/giphy.gif"},
-  {title: "confused10", thumbnailURI: "https://media.giphy.com/media/3oEjHFz13et9fQKFb2/giphy.gif"},
-  {title: "confused11", thumbnailURI: "https://media.giphy.com/media/3oEjHFz13et9fQKFb2/giphy.gif"},
-  {title: "confused12", thumbnailURI: "https://media.giphy.com/media/3oEjHFz13et9fQKFb2/giphy.gif"},
+  {title: "Confused", thumbnailURI: "https://media.giphy.com/media/3oEjHFz13et9fQKFb2/giphy.gif"},
+  {title: "Pug Music", thumbnailURI: "https://media.giphy.com/media/hEwST9KM0UGti/giphy.gif"},
+  {title: "Dogs", thumbnailURI: "https://media.giphy.com/media/QGSEGsTr04bPW/giphy.gif"},
+  {title: "Harambe", thumbnailURI: "https://media.giphy.com/media/26ufp0aB26lWMXZss/giphy.gif"},
+  {title: "Spongebob", thumbnailURI: "https://media.giphy.com/media/3o6ZtbDxnBzSkucV8I/source.gif"},
+  {title: "Mic Drop", thumbnailURI: "https://media.giphy.com/media/mVJ5xyiYkC3Vm/giphy.gif"},
+  {title: "Cats", thumbnailURI: "https://media.giphy.com/media/6Jo7Q3flQ1qEg/giphy.gif"},
 ];
 
 export default data;
